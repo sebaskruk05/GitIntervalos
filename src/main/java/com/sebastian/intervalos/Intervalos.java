f57881e8b3449b89474c2b94dce9fff7e5332f24
Tu Nombre <you@example.com>
2025-02-13 19:25:13 +0000

    Versión con Javadoc añadido

--- src/main/java/com/sebastian/intervalos/Intervalos.java
+++ src/main/java/com/sebastian/intervalos/Intervalos.java
@@ -5,14 +5,25 @@
 package com.sebastian.intervalos;
 
 /**
- *
+ * Proyecto de intervalos de números
  * @author sebastian
+ * @since 13/02/2025
+ * @version 1.0
  */
 public class Intervalos {
-
+    /**
+     * Función principal del proyecto
+     * @param args no se utiliza
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    
+    /**
+     * Función tipo String que determinará el intervalo en el que se encuentra un número.
+     * @param k recibirá el número
+     * @return El intervalo en el que se encuentre ese número (A, B o C).
+     */
     public static String ObtenerIntervalo(double k) {
         if (k < -4) {
             final String A = "Intervalo A";
